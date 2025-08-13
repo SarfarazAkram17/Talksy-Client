f5d391cd71f2ffce7b8379392cd11172c71b1490
sarfarazakram576 <sarfaraz.akram055@gmail.com>
2025-08-14 01:59:15 +0600

    update server url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: 'https://talksy-sarfaraz-akram.vercel.app/api',
+    baseURL: 'https://talksy-server.onrender.com/api',
     withCredentials: true
 })
